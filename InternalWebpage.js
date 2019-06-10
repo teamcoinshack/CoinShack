7c4d0842488a2ba5faac5d0adf3906f2e56db851
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-06-10 21:52:50 +0800

    Change WebView to react-native-community/react-native-webview as <WebView/> is going to be removed from the React Native core

--- InternalWebpage.js
+++ InternalWebpage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WebView } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default class InternalWebpage extends React.Component {
   constructor(props) {
