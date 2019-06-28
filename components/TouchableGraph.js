ec6a53941276a546f6ad9a3083f2dbcb83ca663f
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-06-28 16:55:49 +0800

    Add Graph axis labels formatting

--- components/TouchableGraph.js
+++ components/TouchableGraph.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { VictoryChart, VictoryLine, VictoryVoronoiContainer, VictoryTooltip } from 'victory-native';
+import { VictoryChart, VictoryLine, VictoryVoronoiContainer,
+  VictoryTooltip, VictoryAxis } from 'victory-native';
 import { Dimensions, StyleSheet, View, } from 'react-native';
 import MyBar from './MyBar.js';
 
@@ -17,7 +18,11 @@ export default class TouchableGraph extends Component {
     };
 
     this.fetchStockPrices = this.fetchStockPrices.bind(this);
+    this.getDDMM = this.getDDMM.bind(this);
+    this.getHHMM = this.getHHMM.bind(this);
     this.getFormattedDate = this.getFormattedDate.bind(this);
+    this.getXAxisLabel = this.getXAxisLabel.bind(this);
+    this.getYAxisLabel = this.getYAxisLabel.bind(this);
   }
 
   componentDidMount() {
@@ -62,13 +67,40 @@ export default class TouchableGraph extends Component {
     }
   }
 
+  getDDMM(unixTime) {
+    let date = new Date(unixTime);
+    // getMonth() starts from 0, Jan = 0
+    return `${date.getDate()}/${date.getMonth() + 1}`;
+  }
+
+  getHHMM(unixTime) {
+    let date = new Date(unixTime);
+    return `${String(date.getHours()).padStart(2, 0)}:${String(date.getMinutes()).padStart(2, 0)}`;
+  }
+
   getFormattedDate(unixTime) {
     let date = new Date(unixTime);
     // getMonth() starts from 0, Jan = 0
-    return `${date.getDate()}/${date.getMonth() + 1} ${date.getHours()}:${date.getMinutes()}`;
+    return `${date.getDate()}/${date.getMonth() + 1} ` +
+      `${String(date.getHours()).padStart(2, 0)}:${String(date.getMinutes()).padStart(2, 0)}`;
   }
 
-  // current staic graph
+  getXAxisLabel(unixTime) {
+    if (this.props.days === 1) {
+      return this.getHHMM(unixTime);
+    } else {
+      return this.getDDMM(unixTime);
+    }
+  }
+
+  getYAxisLabel(amount) {
+    if (amount >= 1000) {
+      return "$" + Math.round(amount / 1000) + "K";
+    } else {
+      return "$" + amount;
+    }
+  }
+
   render() {
     if (this.state.isLoading) {
       return (
@@ -83,6 +115,9 @@ export default class TouchableGraph extends Component {
     // NOW TESTING FIRST
     return (
       <VictoryChart
+        height={this.props.height}
+        width={this.props.width}
+        domainPadding={{ x: [20, 0], y: [20, 0] }}
         containerComponent={
           <VictoryVoronoiContainer
             labels={d => {
@@ -90,7 +125,14 @@ export default class TouchableGraph extends Component {
             labelComponent={<VictoryTooltip cornerRadius={0} flyoutStyle={{fill: "white"}}/>}
           />
         }
-      >
+      > 
+        <VictoryAxis
+          tickFormat={x => this.getXAxisLabel(x)}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={y => this.getYAxisLabel(y)} // should probably manually change x axis labels depending on graphDays
+        />
         <VictoryLine
           data={this.state.data} // need to change data to fit x and y
           x={0}
