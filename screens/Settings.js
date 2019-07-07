eea7309807c7c6871f26db486037884fa313a1aa
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-07-07 14:23:54 +0800

    Add Facebook linking for Google sign in users

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {
   Dimensions,
-  StyleSheet, 
+  StyleSheet,
   ScrollView,
+  Alert,
 } from 'react-native';
 import Firebase from 'firebase';
 import db from '../Database.js';
+import MyButton from '../components/MyButton.js';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+
 
 const background = '#373b48';
 
@@ -15,9 +19,16 @@ export default class Settings extends Component {
 
     this.state = {
       id: null,
-    }
+    };
+
     this.logout = this.logout.bind(this);
+    this.getAuthProvider = this.getAuthProvider.bind(this);
     this.isEmailLogin = this.isEmailLogin.bind(this);
+    this.isGoogleLogin = this.isGoogleLogin.bind(this);
+    this.linkFbAcc = this.linkFbAcc.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.changePasswordButton = this.changePasswordButton.bind(this);
+    this.linkFbButton = this.linkFbButton.bind(this);
   }
 
   async componentDidMount() {
@@ -32,7 +43,7 @@ export default class Settings extends Component {
     }
   }
 
-  changePassword = () => {
+  changePassword() {
     this.props.navigation.navigate('ChangePassword');
   }
 
@@ -45,14 +56,68 @@ export default class Settings extends Component {
             })
   }
 
-  isEmailLogin() {
+  getAuthProvider() {
     let user = Firebase.auth().currentUser;
+    console.log(user.providerData); //
     let provider = user.providerData[0].providerId;
-    return provider === "password";
+    // google.com for google, password for email
+    // console.log(provider);
+    return provider;
   }
 
-  render() {
-    const changePasswordButton = (
+  isEmailLogin() {
+    return this.getAuthProvider() === "password";
+  }
+
+  isGoogleLogin() {
+    return this.getAuthProvider() === "google.com";
+  }
+
+  linkFbAcc() {
+    LoginManager
+      .logInWithReadPermissions(['public_profile', 'user_friends', 'email'])
+      .then(result => {
+        if (result.isCancelled) {
+          Alert.alert("Sign in cancelled", "Please try again!");
+        } else {
+          return AccessToken.getCurrentAccessToken();
+        }
+      })
+      .then(data => {
+        const credential = Firebase
+          .auth
+          .FacebookAuthProvider
+          .credential(data.accessToken);
+        return Firebase
+          .auth()
+          .currentUser
+          .linkWithCredential(credential);
+      })
+      .catch(error => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // TODO: handle fb link errors
+        Alert.alert("Linking failed", errorMessage);
+      });
+
+    // let fbProvider = new Firebase.auth.FacebookAuthProvider();
+    // return Firebase
+    //   .auth()
+    //   .currentUser
+    //   .linkWithPopup(fbProvider)
+    //   .then(result => {
+    //     let credential = result.credential;
+    //     let user = result.user;
+    //     // ....
+    //   })
+    //   .catch(error => {
+    //     // handle error here
+    //     console.log(error);
+    //   });
+  }
+
+  changePasswordButton() {
+    return (
       <MyButton
         text="Change Password"
         onPress={this.changePassword}
@@ -60,13 +125,27 @@ export default class Settings extends Component {
         width={Math.round(Dimensions.get('window').width) * 0.6}
       />
     );
+  }
 
+  linkFbButton() {
+    return (
+      <MyButton
+        text="Link Facebook"
+        onPress={this.linkFbAcc}
+        textColor="#00f9ff"
+        width={Math.round(Dimensions.get('window').width) * 0.6}
+      />
+    );
+  }
+
+  render() {
     return (
       <ScrollView 
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        {this.isEmailLogin() && changePasswordButton}
+        {this.isEmailLogin() && this.changePasswordButton()}
+        {this.isGoogleLogin() && this.linkFbButton()}
         <MyButton
           text="Logout"
           onPress={this.logout}
