72c22b285982c92ad16c8514026b68a694cc3db1
Hol Yin Ho <holyin97@gmail.com>
2019-07-25 20:56:05 +0800

    Progress 0 changed to 0.01

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -12,47 +12,52 @@ import db from '../Database.js';
 
 const tier = [0, 0, 11000, 15000, 20000, 30000, 50000];
 
-export default ProgressBar = props => (
-  <View style={{ flexDirection: 'column' }}>
-    <Text style={styles.header}>Progress</Text>
-    <View style={styles.bar}>
-      <Bar
-        progress={
-          props.titleid >= 6
-            ? 1 // max title
-            : (props.totalValue - tier[props.title_id])
-                / (tier[props.title_id + 1] - tier[props.title_id])
-              || 0 // if NaN, i.e., while loading
-        }
-        height={18}
-        width={null}
-        borderWidth={0}
-        borderRadius={5}
-        color="#00dce8"
-        unfilledColor={rowBackground}
-      />
-    </View>
-    <View style={styles.row}>
-      {props.title_id < 6
-        ? (
-          <View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.titleHeader}>Current title:  </Text>
-              <Title title_id={props.title_id} fontSize={18} />
+export default ProgressBar = props => {
+  const getPercent = (value) => {
+    return isNaN(value) || value < 0 ? 0.01 : value;
+  }
+
+  return (
+    <View style={{ flexDirection: 'column' }}>
+      <Text style={styles.header}>Progress</Text>
+      <View style={styles.bar}>
+        <Bar
+          progress={
+            props.title_id >= 6
+              ? 1 // max title
+              : getPercent((props.totalValue - tier[props.title_id])
+                  / (tier[props.title_id + 1] - tier[props.title_id]))
+          }
+          height={18}
+          width={null}
+          borderWidth={0}
+          borderRadius={5}
+          color="#00dce8"
+          unfilledColor={rowBackground}
+        />
+      </View>
+      <View style={styles.row}>
+        {props.title_id < 6
+          ? (
+            <View>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.titleHeader}>Current title:  </Text>
+                <Title title_id={props.title_id} fontSize={18} />
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.titleHeader}>Next title:  </Text>
+                <Title title_id={props.title_id + 1} fontSize={18} />
+              </View>
+              <Text style={{ color: '#dbdbdb', fontSize: 18 }}>
+                Earn ${db.stringify((tier[props.title_id + 1] - props.totalValue).toFixed(2))} more to attain the next title!
+              </Text>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.titleHeader}>Next title:  </Text>
-              <Title title_id={props.title_id + 1} fontSize={18} />
-            </View>
-            <Text style={{ color: '#dbdbdb', fontSize: 18 }}>
-              Earn ${db.stringify((tier[props.title_id + 1] - props.totalValue).toFixed(2))} more to attain the next title!
-            </Text>
-          </View>
-        )
-        : (<Title title_id={props.title_id} />)}
+          )
+          : (<Title title_id={props.title_id} />)}
+      </View>
     </View>
-  </View>
-);
+  );
+}
 
 ProgressBar.defaultProps = {
   title_id: 1
